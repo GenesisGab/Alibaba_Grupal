1dd3e19528f51d15db33ad19e45c595cfe612b62
Gaby <ggasubuyuj@scl.edu.gt>
2022-07-31 23:56:14 -0600

    Se completo la parte del panel

--- js/componentes/panel.js
+++ js/componentes/panel.js
@@ -1,1 +1,27 @@
 /** GÉNESIS */
+
+//CREACION DEL PANEL
+export function panel (){
+    let root = document.querySelector("#despliegue");
+  
+   //CREACION DE LOS DIV PARA EL PANEL
+    root.innerHTML = 
+        '<div class="despliegue_subtitulo">Set de datos</div>'+
+        '<div class="elementos" id="bt_ver">User</div>'+
+        '<div class="elementos">User has creditcard</div>'+
+        '<div class="elementos">Option</div>'+
+        '<div class="elementos">Category</div>'+
+        '<div class="elementos">Orders</div>'+
+        '<div class="elementos">Orders has products</div>'+ 
+        '<div class="elementos">Orders paid creditcard</div>'+
+        '<div class="elementos">Orders placed user</div>'+
+        '<div class="elementos">Products</div>'+
+        '<div class="elementos">Products belong category</div>'+
+        '<div class="elementos">Products belong category</div>'+
+        '<div class="elementos">Products has options</div>'+
+        '<div class="elementos">Carts has products</div>'+
+        '<div class="elementos">Shopingcart</div>'+
+        '<div class="elementos">Vendor</div>';
+}
+
+panel(); //<-- Se llama al panel
---
+++ js/data/products.js
@@ -0,0 +1,96 @@
+/* MADE FOR GENESIS */
+
+export let products = [
+
+    //product_id	product_name	descriptions
+    
+    {
+        product_id: 1200,	
+        product_name: 'Macbook Pro (2017)',                                  	    
+        descriptions: 'The ultimate pro notebook. MacBook Pro features faster processors ;upgraded memory;the Apple T2 chip;and a Retina display with True Tone technology.',
+    },
+    
+    {
+        product_id: 1300,	
+        product_name: 'Macbook Air (2015)',                                   	 
+        descriptions: 'MacBook Air lasts up to an incredible 12 hours between charges So from your morning coffee till your evening commute;you can work unplugged. When itís time to kick back and relax;you can get up to 12 hours of iTunes movie playback.'
+    },
+
+    {
+        product_id: 1400,	
+        product_name: 'Iphone X',	                                                 
+        descriptions: 'The iPhone X display is so immersive the device itself disappears into the experience.',
+    },
+
+    {
+        product_id: 1500,	
+        product_name: 'Iphone 7',	                                                 
+        descriptions: 'Great connectivity of this device includes Bluetooth 4.2 version with A2DP'
+    },
+
+    {
+        product_id: 1600,	
+        product_name: 'Iphone 8',	                                                 
+        descriptions: 'iPhone 8 introduces a glass design. The glass back enables easy wireless charging.'
+    },
+
+    {
+        product_id: 1700,	
+        product_name: 'Ipad Air',	                                                 
+        descriptions: '4th gen The iPad Air is unbelievably thin and light. And yet it is so much more powerful and capable'
+    },
+
+    {
+        product_id: 1800,	
+        product_name: 'Ipad Mini 3th gen',	                                     
+        descriptions: '3th gen Everything you love about iPad ó the beautiful screen and fast'
+    },
+
+    {
+        product_id: 1900,	
+        product_name: 'ESC8000 G3',	                                             
+        descriptions: 'G3 High-density GPU server with hybrid computing power. ASUS-patented Adaptable Topology design.'
+    },
+
+    {
+        product_id: 2000,	
+        product_name: 'ESC8000 G4',	                                             
+        descriptions: 'G4 High performance ASUS 2U server with hybrid-storage design and high power-efficiency'
+    },
+
+    {
+        product_id: 2100,	
+        product_name: 'XPS 13 - 5080',	                                         
+        descriptions: 'Thinner and more powerful than ever the Dell XPS reinforces its lofty standing with an 8th Gen Intel Core processor immaculate 4K UHD display, and super-slim build.'
+    },
+
+    {
+        product_id: 2200,	
+        product_name: 'XPS 15 - 5070',	                                         
+        descriptions: 'Ultra-thin and distinctly refined the stylish Dell Inspiron gives definitive elegance to a powerful and expansive PC experience.'
+    },
+
+    {
+        product_id: 2300,	
+        product_name: 'Monoprice Ultra Slim Series High Speed HDMI Cable',	     
+        descriptions: 'The Monoprice Ultra Slim Active High Speed HDMI Cable series is designed with the thinnest TVs in mind'
+    },
+
+    {
+        product_id: 2400,	
+        product_name: 'Monoprice Ultra Slim Series High Speed HDMI Cable - 4K',	 
+        descriptions: 'Monoprice Commercial Cable supports the following HDMI features: 4K resolution at 24Hz. 3D video.'
+    },
+    {
+        product_id: 2500,	
+        product_name: 'Avantree HT3189',                                           
+        descriptions: 'Wireless Headphones	Avantree HT3189 Wireless Headphones for TV Watching & PC Gaming with Bluetooth'
+    },
+
+    {
+        product_id: 2600,	
+        product_name: 'COWIN E7 PRO',	                                             
+        descriptions: 'Active Noise Cancelling Headphone Bluetooth Headphones with Microphone Hi-Fi Deep Bass Wireless Headphones Over Ear 30H Playtime for Travel Work TV Computer Phone'
+    },
+
+]
